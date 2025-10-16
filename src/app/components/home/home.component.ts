289342d9b94a1629f0ccade9cbe7b96d0ca4b3d0
Adler <adler.alves@hotmail.com>
2025-10-16 18:19:21 -0300

    adicionando carrossel e informacoes na home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  currentSlide = 0;
+  slides = [
+    { id: 0, title: 'Pureza em Cada Ingrediente', content: 'Ingredientes naturais selecionados' },
+    { id: 1, title: 'Sustentabilidade em Primeiro Lugar', content: 'Soluções que respeitam a natureza' },
+    { id: 2, title: 'Excelência em Cada Produto', content: 'Qualidade garantida em todos os produtos' }
+  ];
+  autoSlideInterval: any;
 
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  previousSlide() {
+    this.currentSlide = this.currentSlide === 0 ? this.slides.length - 1 : this.currentSlide - 1;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
+
+  startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Change slide every 5 seconds
+  }
+
+  stopAutoSlide() {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+    }
+  }
 }
